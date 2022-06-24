f007810be78892ab6d2040c922f4b12910bc400c
cactoes <Maartenhvanrooij@gmail.com>
2022-06-24 22:58:13 +0200

    slight cleanup

--- html/main/renderer.js
+++ html/main/renderer.js
@@ -1,17 +1,27 @@
 const { ipcRenderer } = require("electron")
 
+// config for local access
 let config = {}
+// send request to ipcmain to retrieve to config
 const call_update_config = () => ipcRenderer.send("getConfig")
+// have we setup the ui
 let firstTimeSetup = false
 
+// when ipcmain send us config data do ...
 ipcRenderer.on("config", (e, data) => {
+  // update our local config
   config = data
+  // setup ui if we havent already
   if (!firstTimeSetup)
     setup_ui_on_update()
 })
 
+// the ui setup/initialise function
 const setup_ui_on_update = async () => {
+  // set to true so we dont do setup again
   firstTimeSetup = true
+
+  // set the active lane in th ui from config
   for (var i = 0; i < 5; i++) {
     const imgPrefix = AllLanesOneImg[i].src.split("/")
     if ( imgPrefix[imgPrefix.length - 1] == `Position_Challenger-${config.auto.champion.defaultLane}.png`) {
@@ -19,6 +29,7 @@ const setup_ui_on_update = async () => {
     }
   }
 
+  // set the active lane in th ui from config
   for (var i = 0; i < 5; i++) {
     const imgPrefix = AllLanesTwoImg[i].src.split("/")
     if ( imgPrefix[imgPrefix.length - 1] == `Position_Challenger-${config.auto.spells.defaultLane}.png`) {
@@ -26,10 +37,9 @@ const setup_ui_on_update = async () => {
     }
   }
 
+  // setup all values from config
   document.getElementById("checkLaneChampion").parentElement.className = `lanes__one noselect ${config.auto.champion.checkLane? "":"disabled"}`
   document.getElementById("checkLaneSpells").parentElement.className = `lanes__two noselect ${config.auto.spells.checkLane? "":"disabled"}`
-  document.getElementById("enableScripts").className = `${config.misc.script? "":"disabled2"}`
-  document.getElementById("autoAccept").className = `${config.auto.acceptMatch? "":"disabled2"}`
 
   document.getElementById("autoPick").className = config.auto.champion.set? "":"disabled2"
   document.getElementById("autoLock").className = config.auto.champion.lock? "":"disabled2"
@@ -37,41 +47,36 @@ const setup_ui_on_update = async () => {
 
   document.getElementById("autoRunes").className = config.auto.runes.set? "":"disabled2"
   document.getElementById("runesPrefix").innerHTML = config.auto.runes.prefix
+
   document.getElementById("status").innerHTML = config.misc.status
   document.getElementById("tier").innerHTML = config.misc.rank.tier
   document.getElementById("rank").innerHTML = config.misc.rank.rank
+  document.getElementById("enableScripts").className = `${config.misc.script? "":"disabled2"}`
+  document.getElementById("autoAccept").className = `${config.auto.acceptMatch? "":"disabled2"}`
 }
 
+// for when we want to send something to ipcmain
 const ipc_send = (dest, data) => {
   ipcRenderer.send(dest, data)
 }
 
+// global dom elements
 const allClose = document.getElementsByClassName("client_close")
-for (var i = 0; i < allClose.length; i++) {
-  allClose[i].addEventListener("click", () => {ipc_send("closeWindow")})
-}
-
 const allMini = document.getElementsByClassName("client_mini")
-for (var i = 0; i < allClose.length; i++) {
-  allMini[i].addEventListener("click", () => {ipc_send("miniWindow")})
-}
-
 const allClient_status = document.getElementsByClassName("client_status")
-
-ipcRenderer.on("logged_in", (e, state) => {
-  if (state) {
-    for (var i = 0; i < allClient_status.length; i++) {
-      allClient_status[i].style = "background-color: rgba(93, 209, 103, 1); animation: none;"
-    }
-  } else {
-    for (var i = 0; i < allClient_status.length; i++) {
-      allClient_status[i].style = "background-color: rgba(209, 93, 93, 1); animation: pulse 1s infinite alternate linear;"
-    }
-  }
-})
-
 const allArrows = document.getElementsByClassName("arrow")
 
+// setup all close buttons
+for (var i = 0; i < allClose.length; i++) {
+  allClose[i].addEventListener("click", () => { ipc_send("closeWindow") })
+}
+
+// setup all minimize buttons
+for (var i = 0; i < allMini.length; i++) {
+  allMini[i].addEventListener("click", () => { ipc_send("miniWindow") })
+}
+
+// setup the left & right arrow
 for (var i = 0; i < allArrows.length; i++) {
   allArrows[i].addEventListener("click", (e) => {
     const direction = e.target.classList[1]
@@ -102,25 +107,59 @@ for (var i = 0; i < allArrows.length; i++) {
   })
 }
 
+// if ipcmain send us a signal that we are (not/no longer) logged in we update the ui
+ipcRenderer.on("logged_in", (e, state) => {
+  if (state) {
+    for (var i = 0; i < allClient_status.length; i++) {
+      allClient_status[i].style = "background-color: rgba(93, 209, 103, 1); animation: none;"
+    }
+  } else {
+    for (var i = 0; i < allClient_status.length; i++) {
+      allClient_status[i].style = "background-color: rgba(209, 93, 93, 1); animation: pulse 1s infinite alternate linear;"
+    }
+  }
+})
+
+// dom elements for the lane imgs
 const AllLanesOneImg = document.getElementsByClassName("lanes__one")[0].querySelectorAll("img")
 const AllLanesTwoImg = document.getElementsByClassName("lanes__two")[0].querySelectorAll("img")
 
+// make lanes ui interactable
 for (var i = 0; i < AllLanesOneImg.length; i++) {
   AllLanesOneImg[i].addEventListener("click", (e) => {
+    // reset all the elements
     for (var i = 0; i < AllLanesOneImg.length; i++) {
       AllLanesOneImg[i].className = ""
+      // enable the one that was clicked
       e.target.className = "active_img"
     }
   })
   AllLanesTwoImg[i].addEventListener("click", (e) => {
+    // reset all the elements
     for (var i = 0; i < AllLanesTwoImg.length; i++) {
       AllLanesTwoImg[i].className = ""
+      // enable the one that was clicked
       e.target.className = "active_img"
     }
   })
 }
 
-document.getElementById("saveDefaultLanes").addEventListener("click", () => {
+// click events for the ui
+document.getElementById("checkLaneChampion").addEventListener("click", (e) => {
+  if (e.target.parentElement.className == "lanes__one noselect disabled") {
+    e.target.parentElement.className = "lanes__one noselect"
+  } else {
+    e.target.parentElement.className = "lanes__one noselect disabled"
+  }
+})
+document.getElementById("checkLaneSpells").addEventListener("click", (e) => {
+  if (e.target.parentElement.className == "lanes__two noselect disabled") {
+    e.target.parentElement.className = "lanes__two noselect"
+  } else {
+    e.target.parentElement.className = "lanes__two noselect disabled"
+  }
+})
+document.getElementById("saveDefaultLanes").addEventListener("click", (e) => {
   let activeChampion = -1
   const currentLaneOne = document.getElementsByClassName("lanes__one")[0].querySelectorAll("img")
 
@@ -146,36 +185,10 @@ document.getElementById("saveDefaultLanes").addEventListener("click", () => {
   call_update_config()
 })
 
-document.getElementById("checkLaneChampion").addEventListener("click", (e) => {
-  if (e.target.parentElement.className == "lanes__one noselect disabled") {
-    e.target.parentElement.className = "lanes__one noselect"
-  } else {
-    e.target.parentElement.className = "lanes__one noselect disabled"
-  }
-})
-
-document.getElementById("checkLaneSpells").addEventListener("click", (e) => {
-  if (e.target.parentElement.className == "lanes__two noselect disabled") {
-    e.target.parentElement.className = "lanes__two noselect"
-  } else {
-    e.target.parentElement.className = "lanes__two noselect disabled"
-  }
-})
-
 document.getElementById("autoPick").addEventListener("click", (e) => {
   if (e.target.nodeName == "P")
     e.target.parentElement.className =  e.target.parentElement.className == "disabled2"? "":"disabled2"
 })
-
-document.getElementById("enableScripts").addEventListener("click", (e) => {
-  if (e.target.nodeName == "P")
-    e.target.className = e.target.className == "disabled2"? "":"disabled2"
-})
-document.getElementById("autoAccept").addEventListener("click", (e) => {
-  if (e.target.nodeName == "P")
-    e.target.className = e.target.className == "disabled2"? "":"disabled2"
-})
-
 document.getElementById("autoLock").addEventListener("click", (e) => {
   if (e.target.nodeName == "P")
     e.target.parentElement.className = e.target.parentElement.className == "disabled2"? "":"disabled2"
@@ -184,11 +197,22 @@ document.getElementById("autoBan").addEventListener("click", (e) => {
   if (e.target.nodeName == "P")
   e.target.parentElement.className =  e.target.parentElement.className == "disabled2"? "":"disabled2"
 })
+
+document.getElementById("savePicks").addEventListener("click", (e) => {
+  const autoPick = !(document.getElementById("autoPick").className == "disabled2")
+  const autoLock = !(document.getElementById("autoLock").className == "disabled2")
+  const autoBan = !(document.getElementById("autoBan").className == "disabled2")
+
+  ipc_send("savePicks", {
+    autoPick, autoLock, autoBan
+  })
+
+  call_update_config()
+})
 document.getElementById("autoRunes").addEventListener("click", (e) => {
   e.target.className = e.target.className == "disabled2"? "":"disabled2"
 })
-
-document.getElementById("saveRunes").addEventListener("click", () => {
+document.getElementById("saveRunes").addEventListener("click", (e) => {
   const autoRunes = !(document.getElementById("autoRunes").className == "disabled2")
   const runesPrefix = document.getElementById("runesPrefix").innerHTML
   
@@ -199,7 +223,15 @@ document.getElementById("saveRunes").addEventListener("click", () => {
   call_update_config()
 })
 
-document.getElementById("saveMisc").addEventListener("click", () => {
+document.getElementById("enableScripts").addEventListener("click", (e) => {
+  if (e.target.nodeName == "P")
+    e.target.className = e.target.className == "disabled2"? "":"disabled2"
+})
+document.getElementById("autoAccept").addEventListener("click", (e) => {
+  if (e.target.nodeName == "P")
+    e.target.className = e.target.className == "disabled2"? "":"disabled2"
+})
+document.getElementById("saveMisc").addEventListener("click", (e) => {
   const scripts = !(document.getElementById("enableScripts").className == "disabled2")
   const status = document.getElementById("status").innerHTML
   const rank = {
@@ -209,18 +241,6 @@ document.getElementById("saveMisc").addEventListener("click", () => {
   const autoAccept = !(document.getElementById("autoAccept").className == "disabled2")
   ipc_send("saveMisc", {
     scripts, status, rank, autoAccept
-  })
-
-  call_update_config()
-})
-
-document.getElementById("savePicks").addEventListener("click", () => {
-  const autoPick = !(document.getElementById("autoPick").className == "disabled2")
-  const autoLock = !(document.getElementById("autoLock").className == "disabled2")
-  const autoBan = !(document.getElementById("autoBan").className == "disabled2")
-
-  ipc_send("savePicks", {
-    autoPick, autoLock, autoBan
   })
 
   call_update_config()
